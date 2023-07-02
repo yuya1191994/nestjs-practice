6bb1bb82e8003d8ecc82ad2474289f41214c0c9f
yuya1191994 <51263536+yuya1191994@users.noreply.github.com>
2023-07-02 14:53:19 +0900

    create REST API "dogs" create with "nest g resource"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [],
+  imports: [DogsModule,],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService
+  ],
 })
-export class AppModule {}
+export class AppModule { }
